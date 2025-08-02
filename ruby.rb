d118efb10d4a25b58a850de04e328be2d8b1aae3
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2025-08-01 21:09:24 -0500

    completed caesar_cipher

--- ruby.rb
+++ ruby.rb
@@ -1,44 +1,49 @@
 print "Enter a string: "
-string = gets
+string = gets.chomp
 print "Enter your desired shift: "
 shift = gets.to_i
 
 
 def caesar_cipher(str, shift_num)
-  temp_arr = []
+  track_capitals_array = []
+  track_non_alpha = {}
+
+  # Finds where the capital letters in the string are
   str.each_char.with_index do |val, idx|
     if val != val.downcase
-      temp_arr.push(idx)
+      track_capitals_array.push(idx)
     end
   end
 
+  # Finds any nonalphabetical letters and their positions, saves them to a hash, then takes them out of the string
+  str.each_char.with_index do |chr, chr_index|
+    if !chr.downcase.ord.between?(97, 122)
+      track_non_alpha[chr_index] = chr
+      str.slice!(chr)
+    end
+  end
+
+  # Lowercase all letters and split into array
   new_str = str.downcase!.split("")
+
+  # Turns all letters into ascii value
   new_str.map! {|val| val.ord}
+
+  # Adds shift to all letters
   new_str.map! {|val| val+= shift_num}
+
+  # Turns all ascii values back into letters
   new_str.map! {|val| ((val - 97) % 26 + 97).chr}
 
-  str = new_str.inspect
-print str
+  str = new_str.join
+
+  # Uses hash from eariler to re-insert all nonalphabetical letters
+  track_non_alpha.each_pair {|key, val| str.insert(key, val)}
+
+  # Uses array that help postion of capital letters to re-capitalize letters
+  track_capitals_array.each {|cap_letter| str[cap_letter] = str[cap_letter].upcase!}
+  
+  puts str
 end
 
 caesar_cipher(string, shift)
-
-
-# how to shift alphabet and wrap around
-def shift_alphabet(array, shift)
-  new_alphabet = []
-  array.map {|str| new_alphabet.push(str.ord)}
-  print "org array: #{array}\n"
-
-  new_alphabet.map! {|val| val+= shift}
-
-  new_alphabet.map! {|val| ((val - 97) % 26 + 97).chr}
-  puts "mod array: #{new_alphabet}\n"
-
-end
-
-
-
-
-
-
